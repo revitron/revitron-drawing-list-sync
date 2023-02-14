ad6096143a3b1062c56cbccad730d90acc1e5847
j.hoell <j.hoell@herzogdemeuron.com>
2023-02-14 01:04:25 +0100

    fix us date csv conversion

--- lib/drawinglistsync/sheets.py
+++ lib/drawinglistsync/sheets.py
@@ -1,5 +1,6 @@
 from revitron import _, DB, DOC
-
+import date
+import re
 
 def createOrUpdateSheets(drawingList, revisionList, modelSheetCollection, config):
 	for item in drawingList.all():
@@ -13,6 +14,9 @@ def createOrUpdateSheets(drawingList, revisionList, modelSheetCollection, config
 		if _(sheet).isNotOwned():
 			for key, value in data.items():
 				if _(sheet).getParameter(key).exists() or config.createMissingParameters:
+					# conversion of US formated dates from CSV to defined format
+					if date.isUSDate(value):
+							value = date.normalizeDateString(value, date.DATE_FORMATS[0])
 					_(sheet).set(key, value)
 			sheetRevisions = revisionList.get(number)
 			if sheetRevisions:
