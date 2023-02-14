32059b7b1e6678f5d3699d73bf9c8a72a8bdbc9a
j.hoell <j.hoell@herzogdemeuron.com>
2023-02-14 01:03:09 +0100

    fix us date conversion from csv

--- lib/drawinglistsync/date.py
+++ lib/drawinglistsync/date.py
@@ -1,12 +1,14 @@
 import datetime
+import re
 
 DATE_REGEX = '(\d{1,2}\.\d{1,2}\.\d{4}|\d{6}|\d{4}-\d{2}-\d{2})'
+DATE_US_REGEX = '(\d{1,2}\/\d{1,2}\/\d{4})'
 DATE_FORMATS = [
     r'%d.%m.%Y',  # 31.01.2023
     r'%y%m%d',  # 230131
     r'%Y-%m-%d',  # 2023-01-31
+    r'%m/%d/%Y', # 12/31/2022
 ]
-
 
 def getDateFromString(dateString):
 	for format in DATE_FORMATS:
@@ -17,9 +19,13 @@ def getDateFromString(dateString):
 		except:
 			pass
 
-
 def normalizeDateString(dateString, outputFormat):
 	date = getDateFromString(dateString)
 	if date:
 		return date.strftime(outputFormat)
 	return ''
+
+def isUSDate(dateString):
+	if re.match('^' + DATE_US_REGEX + '(.*)$', dateString):
+		return True
+	return False
